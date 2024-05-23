39c2e95e0f5209b1ed6f7259514a2ae42b1d67df
mapulecodes <mbuzwanamapule@gmail.com>
2024-05-23 14:57:05 +0200

    23.05

---
+++ script/code.js
@@ -0,0 +1,13 @@
+function greet(mapule) {
+    console.log("Hello", mapule);
+}
+
+function multiply(a, b) {
+    console.log("Multiplication result:", a * b);
+}
+
+function tableOfFive(number) {
+    let result = number * 5;
+    console.log("Result:", result);
+    return result;
+}
